43a9d36387c66bef4640f2dac977d1356f631779
RatovosonPascal <lratovoson@gmail.com>
2024-06-07 13:55:48 +0200

    first push

---
+++ src/main/java/com/Pascal/Financial/Managment/FinancialManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.Pascal.Financial.Managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pascal/Financial/Managment/FinancialManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pascal.Financial.Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
